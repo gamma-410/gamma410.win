e32d1953cb4e33c49d74ddb8ef0a47a24aee6b61
gamma <mail@dr-notes.work>
2022-04-05 01:47:42 +0900

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,14 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'gamma.' },
+      { hid: 'og:type', property: 'og:type', content: 'Software Developer' },
+      { hid: 'og:url', property: 'og:url', content: 'https://dr-notes.work' },
+      { hid: 'og:title', property: 'og:title', content: 'gamma.' },
+      { hid: 'og:description', property: 'og:description', content: 'Software Developer' },
+      { hid: 'og:image', property: 'og:image', content: 'https://dr-notes.work/ogp.png' },
+      { name: 'twitter:card', content: 'summary' }//　twitterの画像サイズ
     ],
     link: [
       { rel: 'icon', type: 'image/icon', href: '/logo.png' },
